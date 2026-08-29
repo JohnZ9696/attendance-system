c33734bb5f9bf37992e4049ed6078e5435050ab2
DBin2102 <minhvan9602@gmail.com>
2026-08-29 15:05:45 +0700

    feat: implement assistance creation endpoint and DTO

--- esp32-service/src/wifi_config.h
+++ esp32-service/src/wifi_config.h
@@ -1,7 +1,7 @@
 #ifndef WIFI_CONFIG_H
 #define WIFI_CONFIG_H
 
-#define WIFI_SSID     "Minh Thu"
-#define WIFI_PASSWORD "camsaike"
+#define WIFI_SSID     "Thuenhatot_B207"
+#define WIFI_PASSWORD "Nhadepsachse"
 
 #endif // WIFI_CONFIG_H
