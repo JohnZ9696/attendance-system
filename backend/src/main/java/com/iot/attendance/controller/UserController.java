5b8241447ae9d8c165e1305cc9811252e7e703f0
DBin2102 <minhvan9602@gmail.com>
2026-09-06 22:59:43 +0700

    fix: update delete user logic to deactivate instead of permanent removal

--- backend/src/main/java/com/iot/attendance/controller/UserController.java
+++ backend/src/main/java/com/iot/attendance/controller/UserController.java
@@ -73,11 +73,11 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable UUID id) {
-        if (!repository.existsById(id)) {
-            return ResponseEntity.notFound().build();
-        }
-        repository.deleteById(id);
-        return ResponseEntity.noContent().build();
+        return repository.findById(id).map(student -> {
+            student.setIsActive(false);
+            repository.save(student);
+            return ResponseEntity.noContent().build();
+        }).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping(
